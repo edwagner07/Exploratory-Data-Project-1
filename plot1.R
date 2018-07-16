fdae3c5eda9d66fb67880078ac7efdaa4b7ad73a
edwagner07 <40665301+edwagner07@users.noreply.github.com>
2018-07-15 23:07:00 -0500

    Delete plot1.R

--- plot1.R
+++
@@ -1,4 +0,0 @@
-
-png(filename = "plot 1.png",width = 400, height = 400)
-hist(smallpower$Global_active_power, col = "red", breaks = 12, main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
-dev.off()
